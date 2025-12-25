a6938c8b1f270926c86961c4e4cea71c71c40fb6
Michael Luchner <michael.luchner.12+it@gmail.com>
2025-12-25 18:05:30 +0100

    new comment fixed issue

--- Tests/Fork_Light_Barrier_Test.ino
+++ Tests/Fork_Light_Barrier_Test.ino
@@ -16,10 +16,13 @@ void setup()
 
 void loop() 
 {
+  // Static counters to keep track of trigger activations
   static unsigned long CountLeft = 0;
   static unsigned long CountRight = 0;
+
+  // Activate motors at a set PWM speed
   analogWrite(PIN_MOT_IN1, 100);
-  analogWrite(PIN_ MOT_IN3, 100);
+  analogWrite(PIN_MOT_IN3, 100);
 
   if (digitalRead(PIN_TG_L) == 1)
   {
@@ -29,6 +32,7 @@ void loop()
   {
     CountRight++;
   }
+
   Serial.println("Counter Left: CountLeft");
   Serial.println("Counter Right: CountRight");
 }
