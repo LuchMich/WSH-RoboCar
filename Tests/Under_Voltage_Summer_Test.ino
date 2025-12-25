0214908a1b85799d41785aded6d739198a1cceba
Michael Luchner <michael.luchner.12+it@gmail.com>
2025-12-25 18:07:47 +0100

    new comments

--- Tests/Under_Voltage_Summer_Test.ino
+++ Tests/Under_Voltage_Summer_Test.ino
@@ -1,17 +1,22 @@
+//define constants for pin assignments
 #define PIN_BAT_LVL A2
 #define PIN_SUMMER 4
 
 void setup() 
 {
   Serial.begin(300);
+  //initialize pin modes
   pinMode(PIN_SUMMER, OUTPUT);
 }
 
 void loop() 
 {
+  //variable to hold battery level reading
   static unsigned short BatLvl= 0;
+
   BatLvl = analogRead(PIN_BAT_LVL);
   Serial.println(BatLvl);
+
   if (BatLvl <= 725)
     digitalWrite(PIN_SUMMER, 1);
   else
